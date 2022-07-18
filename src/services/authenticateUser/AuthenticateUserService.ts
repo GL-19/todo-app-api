61fcd1235bccc06b5af160521be8d01e75f5ac6b
GL-19 <loiolagl19@gmail.com>
2022-07-18 16:08:47 -0300

    feat: AuthenticateUserService

---
+++ src/services/authenticateUser/AuthenticateUserService.ts
@@ -0,0 +1,48 @@
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { IUsersRepository } from "../../repositories/users/IUsersRepository";
+import { jwt_secret } from "../../shared/config/auth";
+import { AppError } from "../../shared/errors/AppError";
+import { inject, injectable } from "tsyringe";
+
+interface IAuthenticateResponse {
+	user: {
+		name: string;
+		email: string;
+	};
+	token: string;
+}
+
+@injectable()
+class AuthenticateUserService {
+	constructor(
+		@inject("InMemoryUsersRepository")
+		private usersRepository: IUsersRepository
+	) {}
+
+	async execute(email: string, password: string): Promise<IAuthenticateResponse> {
+		const user = await this.usersRepository.findByEmail(email);
+
+		if (!user) {
+			throw new AppError("Email or Password incorrect");
+		}
+
+		const passwordMatch = await compare(password, user.password);
+
+		if (!passwordMatch) {
+			throw new AppError("Email or Password incorrect");
+		}
+
+		const token = sign({ id: user.id }, jwt_secret, { expiresIn: "7d" });
+
+		return {
+			token,
+			user: {
+				name: user.name,
+				email: user.email,
+			},
+		};
+	}
+}
+
+export { AuthenticateUserService };
---
+++ src/shared/config/auth.ts
@@ -0,0 +1,1 @@
+export const jwt_secret = "r9d26s4ad16asJU0u09890";
